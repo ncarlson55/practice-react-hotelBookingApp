742ef3df80a7c43d7e0fe9bff5f5658b3979b4d2
Nico Carlson <nicocarlson285@gmail.com>
2021-05-18 22:18:58 -0500

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import {  Route, Link, Switch } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Rooms from './pages/Rooms'
-import SingleRoom from './pages/SingleRoom'
-import Error from './pages/Error'
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-  <>
-    <Route exact path='/' component={Home}/>
-    <Route exact path="/rooms/" component={Rooms} />
-    <Route exact path="/rooms/:slug" component={SingleRoom} />
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
-  )
+  );
 }
 
 export default App;
